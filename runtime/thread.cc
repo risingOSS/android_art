hopez13/art

Check if we need a deopt after method entry / exit callbacks
We need to check if we need a deopt after method entry / exit callbacks.
We were using the proxy of IsDeoptimized(method) to check if we need a
deopt but that isn't sufficient always. For example, when there is a
frame pop request or other requests that don't explicitly deopt the
method.

Checking for a deopt after method exit callback requires special care
to avoid calling method exit callbacks again from the interpreter after
a deopt. We already have a mechanism to skip method exit listeners so it
just involves updating the shadow frame to skip method exit listeners.

Bug: 206029744
Test: art/test.py

Change-Id: Ib95ab4348e40de345583e0718617879300828cb7

runtime/thread.cc
@@ -134,7 +134,7 @@ namespace art {
 using android::base::StringAppendV;
 using android::base::StringPrintf;
 
-extern "C" NO_RETURN void artDeoptimize(Thread* self);
+extern "C" NO_RETURN void artDeoptimize(Thread* self, bool after_method_exit_cb);
 
 bool Thread::is_started_ = false;
 pthread_key_t Thread::pthread_key_self_;
@@ -3872,7 +3872,7 @@ void Thread::DumpThreadOffset(std::ostream& os, uint32_t offset) {
   os << offset;
 }
 
-void Thread::QuickDeliverException(bool is_method_exit_exception) {
+void Thread::QuickDeliverException(bool after_method_exit_cb) {
   // Get exception from thread.
   ObjPtr<mirror::Throwable> exception = GetException();
   CHECK(exception != nullptr);
@@ -3880,7 +3880,7 @@ void Thread::QuickDeliverException(bool is_method_exit_exception) {
     // This wasn't a real exception, so just clear it here. If there was an actual exception it
     // will be recorded in the DeoptimizationContext and it will be restored later.
     ClearException();
-    artDeoptimize(this);
+    artDeoptimize(this, after_method_exit_cb);
     UNREACHABLE();
   }
 
@@ -3954,7 +3954,7 @@ void Thread::QuickDeliverException(bool is_method_exit_exception) {
             (force_deopt ? nullptr : exception),
             /* from_code= */ false,
             method_type);
-        artDeoptimize(this);
+        artDeoptimize(this, after_method_exit_cb);
         UNREACHABLE();
       } else {
         LOG(WARNING) << "Got a deoptimization request on un-deoptimizable method "
@@ -3970,7 +3970,7 @@ void Thread::QuickDeliverException(bool is_method_exit_exception) {
   // resolution.
   ClearException();
   QuickExceptionHandler exception_handler(this, false);
-  exception_handler.FindCatch(exception, is_method_exit_exception);
+  exception_handler.FindCatch(exception, after_method_exit_cb);
   if (exception_handler.GetClearException()) {
     // Exception was cleared as part of delivery.
     DCHECK(!IsExceptionPending());

risingOSS/android_art

runtime/thread.cc
@@ -134,7 +134,7 @@ namespace art {
 using android::base::StringAppendV;
 using android::base::StringPrintf;
 
-extern "C" NO_RETURN void artDeoptimize(Thread* self);
+extern "C" NO_RETURN void artDeoptimize(Thread* self, bool skip_method_exit_callbacks);
 
 bool Thread::is_started_ = false;
 pthread_key_t Thread::pthread_key_self_;
@@ -3872,7 +3872,7 @@ void Thread::DumpThreadOffset(std::ostream& os, uint32_t offset) {
   os << offset;
 }
 
-void Thread::QuickDeliverException(bool is_method_exit_exception) {
+void Thread::QuickDeliverException(bool skip_method_exit_callbacks) {
   // Get exception from thread.
   ObjPtr<mirror::Throwable> exception = GetException();
   CHECK(exception != nullptr);
@@ -3880,7 +3880,7 @@ void Thread::QuickDeliverException(bool is_method_exit_exception) {
     // This wasn't a real exception, so just clear it here. If there was an actual exception it
     // will be recorded in the DeoptimizationContext and it will be restored later.
     ClearException();
-    artDeoptimize(this);
+    artDeoptimize(this, skip_method_exit_callbacks);
     UNREACHABLE();
   }
 
@@ -3917,7 +3917,7 @@ void Thread::QuickDeliverException(bool is_method_exit_exception) {
             exception,
             /* from_code= */ false,
             method_type);
-        artDeoptimize(this);
+        artDeoptimize(this, skip_method_exit_callbacks);
         UNREACHABLE();
       } else {
         LOG(WARNING) << "Got a deoptimization request on un-deoptimizable method "
@@ -3933,7 +3933,7 @@ void Thread::QuickDeliverException(bool is_method_exit_exception) {
   // resolution.
   ClearException();
   QuickExceptionHandler exception_handler(this, false);
-  exception_handler.FindCatch(exception, is_method_exit_exception);
+  exception_handler.FindCatch(exception, skip_method_exit_callbacks);
   if (exception_handler.GetClearException()) {
     // Exception was cleared as part of delivery.
     DCHECK(!IsExceptionPending());